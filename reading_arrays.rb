05d1a3549474bc08847b552c58c51bb68e1477b3
clairespills <purpleprincess372@gmail.com>
2018-07-25 15:26:29 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,3 +1,5 @@
+
+
 def first_student_by_index
 return student_name [0] 
 end
@@ -5,35 +7,30 @@ end
 puts first_student_by_index
 
 def fourth_student_by_index
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
   return student [3] 
 end
 
 puts fourth_student_by_index
 
 def last_student_by_index
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
   return student [-1]
 end
 
 puts last_student_by_index
 
 def first_student_by_method
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
   return student.first 
 end
 
 puts first_student_by_method
 
 def last_student_by_method
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
   return student.last 
 end
 
 puts last_student_by_method
 
 def first_second_and_third_students
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
   return student [0..2] 
 end
 
